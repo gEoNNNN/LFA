6dfbbee011c691322a85dbcd07f4a6d94619a3aa
gEoNNNN <eugen.ostafi@isa.utm.md>
2024-03-11 14:29:59 +0200

    final update

--- LAB1/test2.py
+++ LAB1/test2.py
@@ -1,85 +1,45 @@
-import random
-class Grammar:
-    def __init__(self, vn, vt, p, s, DFAvn, DFAp):
-        self.Vn = vn
-        self.Vt = vt
-        self.P = p
-        self.S = s
-        self.DFAVn = DFAvn
-        self.DFAP = DFAp
+import networkx as nx
+import matplotlib.pyplot as plt
 
-    def generate_strings(self):
-        words = []
-        while (len(words) != 5):
-            str = "S"
-            while str.lower() != str:
-                str = str.replace(str[len(str) - 1],
-                                  self.P[str[len(str) - 1]][random.randint(0, len(self.P[str[len(str) - 1]]) - 1)])
-            if str not in words:
-                words.append(str)
-        return words
-
-    def to_automaton(self):
-        states = self.DFAVn + ['X']
-        alphabet = self.Vt
-        transition_function = {}
-        transition_function['X'] = {}
-        start = self.S
-        accept = ['X']
-
-        for non_terminal in self.DFAVn:
-            transition_function[non_terminal] = {}
-            for production in self.DFAP.get(non_terminal, []):
-                if len(production) == 1:
-                    transition_function[non_terminal][production] = 'X'
-                elif len(production) == 2:
-                    transition_function[non_terminal][production[0]] = production[1]
-
-        return FiniteAutomaton(states, alphabet, transition_function, start, accept)
+# Your grammar
+states = ['S', 'AB', 'B', 'X']
+alphabet = ['a', 'b']
+transition_function = {'S': ['aAB'], 'AB': ['aB', 'bB', 'bX'], 'B': ['aB', 'bX'], 'X': []}
+start_state = 'S'
+accept_states = ['X']
 
 
-class FiniteAutomaton:
-    def __init__(self, states, alphabet, transition_function, start, accept):
-        self.states = states
-        self.alphabet = alphabet
-        self.transition_function = transition_function
-        self.start = start
-        self.accept = accept
+def create_transition_graph(states, alphabet, transition_function, start_state, accept_states):
+    G = nx.DiGraph()
 
-    def Check(self, word):
-        current_state = self.start
-        for char in word:
-            if char in self.transition_function.get(current_state, {}):
-                current_state = self.transition_function[current_state][char]
-            else:
-                return False
-        return current_state in self.accept
+    # Add nodes for each state
+    for state in states:
+        if state in accept_states:
+            # Mark accept states with a double circle (special drawing)
+            G.add_node(state, peripheries=2)
+        else:
+            G.add_node(state)
+
+    # Add edges based on the transition function
+    for state, transitions in transition_function.items():
+        for transition in transitions:
+            symbol, next_state = transition[0], transition[1:]  # Split into symbol and resulting state
+            G.add_edge(state, next_state, label=symbol)
+
+    # Draw the graph
+    pos = nx.spring_layout(G)  # positions for all nodes
+    nx.draw_networkx_nodes(G, pos, nodelist=states, node_size=700)
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+
+    # Draw labels
+    labels = {n: n for n in states}
+    edge_labels = dict([((u, v,), d['label']) for u, v, d in G.edges(data=True)])
+    nx.draw_networkx_labels(G, pos, labels, font_size=16)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    plt.axis('off')  # Turn off the axis
+    plt.show()  # Display the graph
 
 
-if __name__ == "__main__":
-    # DFA
-    s = 'S'
-    DFAvn = ['S', 'C', 'R']
-    vt = ['a', 'b']
-    DFAp = {'S': ['aR'], 'C': ['a', 'bS'],'R': ['bS', 'aC']}
-
-    # NFA
-    NFAvn = ['S', 'A', 'B', 'C']
-    NFAp = {
-        'S': ['aA', 'aB'],
-        'A': ['bS'],
-        'B': ['aC'],
-        'C': ['a', 'bS'],
-    }
-    grammar = Grammar(NFAvn, vt, NFAp, s, DFAvn, DFAp)
-    generated_words = grammar.generate_strings()
-    print("\nGenerated strings using NFA:")
-    print(generated_words)
-    print("\nCheck if the words are accepted using DFA:")
-    for i in generated_words:
-        print(f"{i} - {grammar.to_automaton().Check(i)}")
-    print("\nChecking random words:")
-    random_words = ['random', 'rege', 'abababababbbbbbbbbb']
-    for i in random_words:
-        print(f"{i} - {grammar.to_automaton().Check(i)}")
-    print()
+# Call the function to create and display the graph
+create_transition_graph(states, alphabet, transition_function, start_state, accept_states)
